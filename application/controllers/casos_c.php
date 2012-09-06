a3714d8938fa57d53321dcd197f4597bff19d8ac
shannonbit <emmanuelle.laguna@gmail.com>
2012-09-05 23:58:45 -0500

    Se creo controlador que llama al modelo de insercion de casos

--- application/controllers/casos_c.php
+++ application/controllers/casos_c.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Actores_c extends CI_Controller {
+class Casos_c extends CI_Controller {
  
       public function __construct(){
            
