5640add79095b3ab3765ffe1686a41fa8f4d9166
ceuami <ceuami@gmail.com>
2012-09-06 23:02:36 -0500

    mTraerCatalogoActos listo y revisado

--- application/models/catalogos_m.php
+++ application/models/catalogos_m.php
@@ -8,6 +8,10 @@ class Catalogos_m extends CI_Model {
 	    $this->load->database();
 	}
 	
+	/* Este modelo trae los datos de un catálogo que sólo tienen un nivel de alcance
+	 * @param $nombreCatalogo
+	 * */
+	
 	public function mTraerDatosCatalogoNombre(){
 		
 		$nombreCatalogo = 'estatusPerpetradorCatalogo';
@@ -30,18 +34,49 @@ class Catalogos_m extends CI_Model {
 		
 	}/* Fin de mTraerDatosCatalogoNombre*/
 	
+	
+	/* Este modelo trae los datos de los catálogos Actos */
+	
 	public function mTraerDatosCatalogoActos(){
-		
-		$nombreCatalogo = 'estatusPerpetradorCatalogo';
-		
+		 		
+		/* Trae todos los daos de actosN1Catalogo*/
 		$this->db->select('*');
-		$this->db->from($nombreCatalogo);
-		
+		$this->db->from('actosN1Catalogo');
 		$consulta = $this->db->get();
 						
 		/* Pasa la consulta a un cadena */
 		foreach ($consulta->result_array() as $row) {
-			$datos[$row['casoId']] = $row;
+			$datos['actosN1Catalogo'] = $row;
+		}
+		
+		/* Trae todos los daos de actosN2Catalogo*/
+		$this->db->select('*');
+		$this->db->from('actosN2Catalogo');
+		$consulta = $this->db->get();
+						
+		/* Pasa la consulta a un cadena */
+		foreach ($consulta->result_array() as $row) {
+			$datos['actosN2Catalogo'] = $row;
+		}
+		
+		/* Trae todos los daos de actosN3Catalogo*/
+		$this->db->select('*');
+		$this->db->from('actosN3Catalogo');
+		$consulta = $this->db->get();
+						
+		/* Pasa la consulta a un cadena */
+		foreach ($consulta->result_array() as $row) {
+			$datos['actosN3Catalogo'] = $row;
+		}
+		
+		/* Trae todos los daos de actosN4Catalogo*/
+		$this->db->select('*');
+		$this->db->from('actosN4Catalogo');
+		$consulta = $this->db->get();
+						
+		/* Pasa la consulta a un cadena */
+		foreach ($consulta->result_array() as $row) {
+			$datos['actosN4Catalogo'] = $row;
 		}
 		
 		/* Regresa la cadena al controlador*/
@@ -50,5 +85,8 @@ class Catalogos_m extends CI_Model {
 		
 		
 	}/* Fin de mTraerDatosCatalogoActos*/
+	
+	
+	
 	
 }
